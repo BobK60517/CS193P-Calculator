d4025dfc3041aa74ddd4c3d2339bf3888391c7c9
Bob Keefe <BobK60517@gmail.com>
2012-06-29 18:45:24 -0500

    Initial Commit

---
+++ Calculator/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  Calculator
+//
+//  Created by Bob Keefe on 6/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CalculatorTests/CalculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorTests.h
+//  CalculatorTests
+//
+//  Created by Bob Keefe on 6/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CalculatorTests : SenTestCase
+
+@end
