a4ab288945bc0316abb0a37a2e3f8829344ff33a
Bob Keefe <BobK60517@gmail.com>
2012-07-01 17:35:47 -0500

    Initial operations coded

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -7,14 +7,23 @@
 //
 
 #import "CalculatorViewController.h"
+#import "CalculatorBrain.h"
 
 @interface CalculatorViewController ()
 @property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
+@property (nonatomic, strong) CalculatorBrain *brain;
 @end
 
 @implementation CalculatorViewController
+
 @synthesize display;
 @synthesize userIsInTheMiddleOfEnteringANumber = _userIsInTheMiddleOfEnteringANumber;
+@synthesize brain = _brain;
+// Lazily instanstiate the _brain
+-(CalculatorBrain *)brain {
+    if (!_brain) _brain = [[CalculatorBrain alloc] init];
+    return _brain;
+}
 
 - (IBAction)digitPressed:(UIButton *)sender {
     NSString *digit = sender.currentTitle;
@@ -26,9 +35,17 @@
     }
 }
 - (IBAction)enterPressed {
+    [self.brain pushOperand:[self.display.text doubleValue]];
+    self.userIsInTheMiddleOfEnteringANumber = NO;
 }
 
 - (IBAction)operationPressed:(UIButton *)sender {
+    if (self.userIsInTheMiddleOfEnteringANumber) {
+        [self enterPressed];
+    }
+    NSString *operation = sender.currentTitle;
+    double result = [self.brain performOperation:operation];
+    self.display.text = [NSString stringWithFormat:@"%g", result];
 }
 
 @end
